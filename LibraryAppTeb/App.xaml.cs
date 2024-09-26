239196868e29029f807323bcc3f46651364a4d1c
Nicola Nicola <nicosia.k@interia.pl>
2024-09-26 09:55:36 +0200

    Zmiana wielkosci aplikacji

--- LibraryAppTeb/App.xaml.cs
+++ LibraryAppTeb/App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 1140;
+            const int newHeight = 460;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
 }
