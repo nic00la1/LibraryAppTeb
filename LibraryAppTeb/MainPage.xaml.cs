a78f1ffb012742e58da6b70a4922fa448e086d8a
Nicola Nicola <nicosia.k@interia.pl>
2024-09-26 10:26:02 +0200

    Poprawiono logikę

--- LibraryAppTeb/MainPage.xaml.cs
+++ LibraryAppTeb/MainPage.xaml.cs
@@ -13,43 +13,57 @@
         private void InitializeBooks()
         {
             books = new Dictionary<int, (string Title, int Quantity)>
-                {
-                    { 1, ("Potop - Henryk Sienkiewicz", 2) },
-                    { 2, ("Pan Tadeusz - Adam Mickiewicz", 1) },
-                    { 3, ("Zbrodnia i Kara - Fiodor Dostojewski", 4) }
-                };
+                    {
+                        { 1, ("Potop - Henryk Sienkiewicz", 2) },
+                        { 2, ("Pan Tadeusz - Adam Mickiewicz", 1) },
+                        { 3, ("Zbrodnia i Kara - Fiodor Dostojewski", 4) }
+                    };
         }
 
         private void OnBorrowButtonClicked(object sender, EventArgs e)
         {
-            if (int.TryParse(borrowEntry.Text, out int bookId) && books.ContainsKey(bookId))
+            if (int.TryParse(borrowEntry.Text, out int bookId))
             {
-                if (books[bookId].Quantity > 0)
+                if (books.ContainsKey(bookId))
                 {
-                    books[bookId] = (books[bookId].Title, books[bookId].Quantity - 1);
-                    DisplayAlert("Sukces", $"Wypożyczono książkę: {books[bookId].Title}", "OK");
+                    if (books[bookId].Quantity > 0)
+                    {
+                        books[bookId] = (books[bookId].Title, books[bookId].Quantity - 1);
+                        DisplayAlert("Dziękujemy", $"Wypożyczono książkę: {books[bookId].Title}. Życzymy miłego czytania!", "OK");
+                    }
+                    else
+                    {
+                        DisplayAlert("Brak!", "Nie mamy tej książki dostępnej w bibliotece.", "OK");
+                    }
                 }
                 else
                 {
-                    DisplayAlert("Błąd", "Brak dostępnych egzemplarzy tej książki.", "OK");
+                    DisplayAlert("Error!", "Nieprawidłowe ID książki.", "OK");
                 }
             }
             else
             {
-                DisplayAlert("Błąd", "Nieprawidłowe ID książki.", "OK");
+                DisplayAlert("Error!", "Niepoprawna wartość. Proszę wpisać liczby.", "OK");
             }
         }
 
         private void OnReturnButtonClicked(object sender, EventArgs e)
         {
-            if (int.TryParse(returnEntry.Text, out int bookId) && books.ContainsKey(bookId))
+            if (int.TryParse(returnEntry.Text, out int bookId))
             {
-                books[bookId] = (books[bookId].Title, books[bookId].Quantity + 1);
-                DisplayAlert("Sukces", $"Oddano książkę: {books[bookId].Title}", "OK");
+                if (books.ContainsKey(bookId))
+                {
+                    books[bookId] = (books[bookId].Title, books[bookId].Quantity + 1);
+                    DisplayAlert("Dziękujemy", $"Dziękujemy za zwrócenie nam książki: {books[bookId].Title}. Zapraszamy ponownie!", "OK");
+                }
+                else
+                {
+                    DisplayAlert("Error!", "Nie ma u nas książki o takim ID.", "OK");
+                }
             }
             else
             {
-                DisplayAlert("Błąd", "Nieprawidłowe ID książki.", "OK");
+                DisplayAlert("Error!", "Niepoprawna wartość. Proszę wpisać liczby.", "OK");
             }
         }
     }
