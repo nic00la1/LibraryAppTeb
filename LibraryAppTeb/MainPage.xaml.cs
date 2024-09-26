4d9abb7fa7c0eeb9ff763b07c68e58760ddfded9
Nicola Nicola <nicosia.k@interia.pl>
2024-09-26 10:37:05 +0200

    dodano binding (polaczenie designu i logiki)

--- LibraryAppTeb/MainPage.xaml.cs
+++ LibraryAppTeb/MainPage.xaml.cs
@@ -2,70 +2,25 @@
 {
     public partial class MainPage : ContentPage
     {
-        private Dictionary<int, (string Title, int Quantity)> books;
-
         public MainPage()
         {
             InitializeComponent();
-            InitializeBooks();
-        }
-
-        private void InitializeBooks()
-        {
-            books = new Dictionary<int, (string Title, int Quantity)>
-                    {
-                        { 1, ("Potop - Henryk Sienkiewicz", 2) },
-                        { 2, ("Pan Tadeusz - Adam Mickiewicz", 1) },
-                        { 3, ("Zbrodnia i Kara - Fiodor Dostojewski", 4) }
-                    };
         }
 
         private void OnBorrowButtonClicked(object sender, EventArgs e)
         {
-            if (int.TryParse(borrowEntry.Text, out int bookId))
+            if (BindingContext is MainPageViewModel viewModel)
             {
-                if (books.ContainsKey(bookId))
-                {
-                    if (books[bookId].Quantity > 0)
-                    {
-                        books[bookId] = (books[bookId].Title, books[bookId].Quantity - 1);
-                        DisplayAlert("Dziękujemy", $"Wypożyczono książkę: {books[bookId].Title}. Życzymy miłego czytania!", "OK");
-                    }
-                    else
-                    {
-                        DisplayAlert("Brak!", "Nie mamy tej książki dostępnej w bibliotece.", "OK");
-                    }
-                }
-                else
-                {
-                    DisplayAlert("Error!", "Nieprawidłowe ID książki.", "OK");
-                }
-            }
-            else
-            {
-                DisplayAlert("Error!", "Niepoprawna wartość. Proszę wpisać liczby.", "OK");
+                viewModel.BorrowBook(borrowEntry.Text);
             }
         }
 
         private void OnReturnButtonClicked(object sender, EventArgs e)
         {
-            if (int.TryParse(returnEntry.Text, out int bookId))
+            if (BindingContext is MainPageViewModel viewModel)
             {
-                if (books.ContainsKey(bookId))
-                {
-                    books[bookId] = (books[bookId].Title, books[bookId].Quantity + 1);
-                    DisplayAlert("Dziękujemy", $"Dziękujemy za zwrócenie nam książki: {books[bookId].Title}. Zapraszamy ponownie!", "OK");
-                }
-                else
-                {
-                    DisplayAlert("Error!", "Nie ma u nas książki o takim ID.", "OK");
-                }
-            }
-            else
-            {
-                DisplayAlert("Error!", "Niepoprawna wartość. Proszę wpisać liczby.", "OK");
+                viewModel.ReturnBook(returnEntry.Text);
             }
         }
     }
-
 }
---
+++ LibraryAppTeb/MainPageViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LibraryAppTeb
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Book> Books { get; set; }
+
+        public MainPageViewModel()
+        {
+            Books = new ObservableCollection<Book>
+            {
+                new Book { Id = 1, Title = "Potop - Henryk Sienkiewicz", Quantity = 2 },
+                new Book { Id = 2, Title = "Pan Tadeusz - Adam Mickiewicz", Quantity = 1 },
+                new Book { Id = 3, Title = "Zbrodnia i Kara - Fiodor Dostojewski", Quantity = 4 }
+            };
+        }
+
+        public void BorrowBook(string bookIdText)
+        {
+            if (int.TryParse(bookIdText, out int bookId))
+            {
+                var book = Books.FirstOrDefault(b => b.Id == bookId);
+                if (book != null && book.Quantity > 0)
+                {
+                    book.Quantity--;
+                    OnPropertyChanged(nameof(Books));
+                }
+            }
+        }
+
+        public void ReturnBook(string bookIdText)
+        {
+            if (int.TryParse(bookIdText, out int bookId))
+            {
+                var book = Books.FirstOrDefault(b => b.Id == bookId);
+                if (book != null)
+                {
+                    book.Quantity++;
+                    OnPropertyChanged(nameof(Books));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Book : INotifyPropertyChanged
+    {
+        private int _quantity;
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
